119257847427d0ca7183156f9e4936bcb372d0ac
ArturTrigo <artur.trigo.drn@gmail.com>
2020-01-09 19:19:58 -0400

    Add files via upload

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemComponent } from '../item/item.component';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
